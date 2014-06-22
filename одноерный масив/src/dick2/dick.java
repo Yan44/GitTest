a061eb3446e2036a86c99e82107970396393ea37
Yan <Yan@192.168.0.101>
2014-06-22 17:57:27 +0600

    ads

---
+++ одноерный масив/src/dick2/dick.java
@@ -0,0 +1,13 @@
+package dick2;
+
+public class dick {
+	
+	public static void main (String args[]){
+		int[] m = {1, 5, 9, 0, 8};
+			for (int x=4;x>-1;x--) 
+			{System.out.println(-m[x]);
+			}
+		}
+	}
+
+
